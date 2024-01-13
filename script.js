9a6f7b4fe018c50778d005472c9a66a9a9f7b67d
Ashish Dhiwar <112108089+Ashishxo@users.noreply.github.com>
2024-01-13 23:25:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ let reset = document.querySelector("#reset");
 reset.addEventListener("click", ()=>{
     turn = 0;
     for(i=0; i<9; i++){
+        boxes[i].removeAttribute("disabled");
         boxes[i].innerHTML = "";
     }
     document.querySelector("#result").innerHTML = "";
@@ -38,6 +39,9 @@ function checkWinner(){
         let concate = pos1 + pos2 + pos3;
         if(concate != ""){
             if(pos1 == pos2 && pos2 == pos3){
+                for(i=0; i<9; i++){
+                    boxes[i].setAttribute("disabled", "");
+                }
                 if(pos1 == "X"){
                     document.querySelector("#result").innerHTML = "Player 1 Won!";
                     return;
